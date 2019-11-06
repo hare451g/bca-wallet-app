27c676617f55ebc6ad09fd1796064c455eee38ed
hendra sadewa <hendra.sadewa@hara.ag>
2019-11-06 21:50:20 +0700

    create auth store

--- app/store/auth.js
+++ app/store/auth.js
@@ -1,9 +1,11 @@
-import {action} from 'easy-peasy';
+import {post} from 'axios';
+import {action, thunk} from 'easy-peasy';
+import {AUTH_URL} from '../configs/api';
 
 const initialState = {
-  username: '',
+  accountNumber: '',
   pin: '',
-  authorization: '',
+  token: '',
   loading: false,
   error: null,
 };
@@ -19,23 +21,45 @@ const reducers = {
 
   onSubmitLogin: action((state, payload) => {
     state.loading = true;
-    state.username = payload.username;
+    state.accountNumber = payload.accountNumber;
     state.pin = payload.pin;
   }),
 
   onLoginFailed: action((state, payload) => {
     state.loading = false;
     state.error = payload.error;
-    state.authorization = '';
+    state.token = '';
   }),
 
   onLoginSuccess: action((state, payload) => {
     state.loading = false;
     state.error = null;
-    state.authorization = payload.authorization;
+    state.token = payload.token;
   }),
 };
 
-const auth = {...initialState, ...reducers};
+const thunks = {
+  submitLogin: thunk(async (actions, payload) => {
+    try {
+      const {pin, accountNumber} = payload;
+      const requestBody = {
+        pin,
+        account_number: accountNumber,
+      };
+
+      const response = await post(`${AUTH_URL}/obtain-token/`, requestBody);
+
+      const {type, authorization} = response.data;
+
+      actions.onLoginSuccess({
+        token: `${type} ${authorization}`,
+      });
+    } catch (error) {
+      actions.onLoginFailed({error});
+    }
+  }),
+};
+
+const auth = {...initialState, ...reducers, ...thunks};
 
 export default auth;
