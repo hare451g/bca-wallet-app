e5d4420e1aea05947ccb88dff0ce80e49dbfe1b1
hendra sadewa <hendra.sadewa@hara.ag>
2019-11-06 21:50:35 +0700

    create private menu and public menu

---
+++ app/containers/WalletMenuList/constants.js
@@ -0,0 +1,11 @@
+const publicMenu = [
+  {text: 'Activation', value: 'activate_wallet'},
+  {text: 'Wallet Address', value: 'show_wallet'},
+];
+
+const privateMenu = [
+  {text: 'Transfer Antar Bank', value: 'transfer_wallet'},
+  {text: 'Riwayat Transaksi', value: 'transaction_history'},
+];
+
+export {publicMenu, privateMenu};
